ca596551679e6707a549af2d8c4a3ecf1365815e
Srishti Tripathi <tripathisrishti.2010@gmail.com>
2025-01-28 10:34:39 +0530

    Update Main.java
    Added missing comments

--- Main.java
+++ Main.java
@@ -4,7 +4,9 @@ class Main{
 	public static void main(String args[]){
 		UserInput in = new UserInput();
 		ArrayFunctions functions = new ArrayFunctions();
-//		functions.oddEven(in.arrayInput());
+		// Calling the even/odd array display method 
+		functions.oddEven(in.arrayInput());
+		// Calling the min difference method
 		int n = functions.minDiff(in.arrayInput());
 		System.out.println("The index is:" +n);
 	}
